6b677206fa1b01f4409df634377eedc77a3f57d2
joshsmonta <joshsmonta@gmail.com>
2024-10-16 09:47:21 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< HEAD
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {NavBarComponent} from './components/nav-bar/nav-bar.component';
 import { ListComponent } from './components/list/list.component'
+=======
+>>>>>>> f9f5c9d (initial commit)
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< HEAD
   imports: [
     RouterOutlet, 
     MatSlideToggleModule, 
     NavBarComponent,
     ListComponent
   ],
+=======
+  imports: [RouterOutlet],
+>>>>>>> f9f5c9d (initial commit)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+<<<<<<< HEAD
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync('noop')]
+=======
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideClientHydration()]
+>>>>>>> f9f5c9d (initial commit)
 };
